50d144603892b82c4c68fb36bc04f415f9dcbc1f
khyox <jse.mnl@gmail.com>
2018-12-19 11:34:39 +0100

    Add native and contaminated strains to mock for challenging ROC
    Changes to be committed:
    	modified:   recentrifuge/__init__.py
    	modified:   recentrifuge/test/TEST.rcf.html
    	modified:   recentrifuge/test/TEST.rcf.xlsx
    	modified:   recentrifuge/test/mock.xlsx
    	modified:   retest
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='recentrifuge',
-    version='0.24.9',
+    version='0.24.10',
     packages=['recentrifuge'],
     url='http://www.recentrifuge.org',
     license='AGPL except krona.js, with its own license by BNBI',
