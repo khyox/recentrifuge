a6bf0b898ec5616bba992fc01056437d52b5086e
khyox <jse.mnl@gmail.com>
2018-12-22 11:00:34 +0100

    Retest: Improve plot of ROC with mintaxa variation and add skip opt
    On branch master
    	modified:   recentrifuge/__init__.py
    	modified:   retest
    	modified:   setup.py

--- recentrifuge/__init__.py
+++ recentrifuge/__init__.py
@@ -33,7 +33,7 @@ __email__ = 'jse.mnl **AT** gmail.com'
 __maintainer__ = 'Jose Manuel Martí'
 __status__ = 'Beta'
 __date__ = 'Dec 2018'
-__version__ = '0.24.13'
+__version__ = '0.24.14'
 
 import sys
 from Bio import SeqIO
