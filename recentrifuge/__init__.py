b7f611d7481d2a24c145d050432bc031b2e34a76
khyox <jse.mnl@gmail.com>
2020-03-28 20:58:12 -0700

    Upgrade Python support to 3.8
    On branch master
    	modified:   .travis.yml
    	modified:   INSTALL.md
    	modified:   LICENSE.txt
    	modified:   rcf
    	modified:   recentrifuge/__init__.py
    	modified:   remock
    	modified:   retaxdump
    	modified:   retest
    	modified:   rextract
    	modified:   setup.py

--- recentrifuge/__init__.py
+++ recentrifuge/__init__.py
@@ -34,7 +34,7 @@ __email__ = 'jse.mnl **AT** gmail.com'
 __maintainer__ = 'Jose Manuel Martí'
 __status__ = 'Beta'
 __date__ = 'March 2020'
-__version__ = '0.28.15'
+__version__ = '0.29.0'
 
 import sys
 from Bio import SeqIO
