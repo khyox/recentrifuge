9c0daffaf9c4633619d825b614fe048b3c2d24e9
khyox <jse.mnl@gmail.com>
2020-09-02 18:56:10 -0700

    Update tests after another NCBI Taxonomy update
    Recently, the taxonomic classification of phylum Bacteroidetes has
    been improved (García-López, 2019) and NCBI has updated the
    Taxonomy consequently.
    
    On branch master
    	modified:   recentrifuge/__init__.py
    	modified:   recentrifuge/test/TEST.rcf.html
    	modified:   recentrifuge/test/TEST.rcf.xlsx
    	modified:   setup.py

--- recentrifuge/__init__.py
+++ recentrifuge/__init__.py
@@ -33,8 +33,8 @@ __license__ = 'GNU Affero General Public License Version 3'
 __email__ = 'jse.mnl **AT** gmail.com'
 __maintainer__ = 'Jose Manuel Martí'
 __status__ = 'Production/Stable'
-__date__ = 'Ago 2020'
-__version__ = '1.1.1'
+__date__ = 'Sep 2020'
+__version__ = '1.1.2'
 
 import sys
 from Bio import SeqIO
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='recentrifuge',
-    version='1.1.1',
+    version='1.1.2',
     packages=['recentrifuge'],
     url='http://www.recentrifuge.org',
     license='AGPL except krona.js, with its own license by BNBI',
