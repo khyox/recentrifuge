ce9b1ae52abb0f0c018ccde788fe59955d4058a9
khyox <jse.mnl@gmail.com>
2021-05-11 17:16:01 -0700

    Update tests after NCBI Taxonomy reorganization
    Changes because of modifications inside Rhizobiaceae family
    of a-proteobacteria.
    
    On branch master
    	modified:   recentrifuge/__init__.py
    	modified:   recentrifuge/config.py
    	modified:   recentrifuge/test/TEST.rcf.html
    	modified:   recentrifuge/test/TEST.rcf.xlsx
    	modified:   setup.py

--- recentrifuge/__init__.py
+++ recentrifuge/__init__.py
@@ -33,8 +33,8 @@ __license__ = 'GNU Affero General Public License Version 3'
 __email__ = 'jse.mnl **AT** gmail.com'
 __maintainer__ = 'Jose Manuel Martí'
 __status__ = 'Production/Stable'
-__date__ = 'Feb 2021'
-__version__ = '1.3.2'
+__date__ = 'May 2021'
+__version__ = '1.3.3'
 
 import sys
 from Bio import SeqIO
